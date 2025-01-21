766811d57c5bbf21a1fb8d725c0f0ca9cfa072f1
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-01-21 17:55:13 -0500

    created db connection for deployment

--- ufindit/backend/db_config.php
+++ ufindit/backend/db_config.php
@@ -1,14 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root"; // the MySQL username
-$password = ""; // the MySQL password
-$dbname = "ufindit";
+// Correcting the URL parsing
+$url = 'mysql://sd71uc4xbtds69l9:jeigr7149b4s4ldm@arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h7ehdbu4aune32sp';
+$dbparts = parse_url($url);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Extract parts
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'], '/');
 
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-
-?>
+echo "Connection was successfully established!";
